39da4cf89e677ca6a1800c3a3f70cdebb2fd47e4
Felipe Vieira <felip.3lima@hotmail.com>
2019-11-26 20:50:21 -0300

    chore: Finished Version

--- src/models/Spot.js
+++ src/models/Spot.js
@@ -16,7 +16,8 @@ const SpotSchema = new mongoose.Schema({
 })
 
 SpotSchema.virtual('thumbnail_url').get(function() {
-    return `http://localhost:3333/files/${this.thumbnail}`
+    const url = process.env.URL || 'https://localhost:3333'
+    return `${url}/files/${this.thumbnail}`
 })
 
 module.exports = mongoose.model('Spot', SpotSchema)
